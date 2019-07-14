bde9fa73bd156f83812038c03192914746e70424
jongseung <park930902@gmail.com>
2019-07-14 18:05:46 +0900

    학생 관리 프로그램 완료
    학생 관리 프로그램 마무리(예외처리 못함)

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -31,12 +31,23 @@ namespace Student_list_Program
             // 학생 목록 생성
             Student[] std_list = new Student[0]; //빈 std_list 생성
 
-            for (; select < 4;)
+            for (; select != 4;)
             {
                 Console.Clear();
                 Console.WriteLine("1. 학생 목록 2. 학생 추가 3.학생 삭제 4. 프로그램 종료"); // 다른 번호를 눌러도 프로그램 종료
                 Console.Write("메뉴 번호를 입력하세요 : ");
-                select = int.Parse(Console.ReadLine()); //사용자 메뉴 선택
+                try
+                {
+                    select = int.Parse(Console.ReadLine()); //사용자 메뉴 선택
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("메뉴 번호를 정확히 입력해 주세요 !");
+                    Console.ResetColor();
+                    Console.ReadKey();
+
+                }
 
                 if (select == 1) // 1. 학생 목록
                 {
@@ -52,18 +63,21 @@ namespace Student_list_Program
                     int new_idx = std_list.Length - 1; //std_list 에 새로 추가할 student 인덱스 값
 
                     std_list[new_idx] = new Student(); //std_list[new_idx]에 새로운 객체 생성
-
                     Console.WriteLine("학생 정보 추가");
                     Console.Write("학생 이름 : ");
                     std_list[new_idx].name = Console.ReadLine();
+
+
                     Console.Write("국어 성적 : ");
                     std_list[new_idx].kor = int.Parse(Console.ReadLine());
                     Console.Write("영어 성적 : ");
                     std_list[new_idx].eng = int.Parse(Console.ReadLine());
                     Console.Write("수학 성적 : ");
                     std_list[new_idx].math = int.Parse(Console.ReadLine());
-                    Console.WriteLine("학생 정보 입력에 성공했습니다.");
 
+
+                    Console.WriteLine("학생 정보 입력에 성공했습니다.");
+                    Console.ReadKey();
                 }
                 else if (select == 3) // 3. 학생 삭제
                 {
@@ -71,14 +85,15 @@ namespace Student_list_Program
 
                     if (std_list.Length < 1)
                     {
-                        continue;
+
+                        Console.ReadKey();
                     }
                     else
                     {
                         int num;
                         do
                         {
-                            Console.Write("삭제할 학생 번호를 입력해주세요 : 뒤로가기[0번] "); // 번호가 작거나 크면 반복실행
+                            Console.Write("삭제할 학생 번호를 입력해주세요 : "); // 번호가 작거나 크면 반복실행
                             num = int.Parse(Console.ReadLine());
                         } while (!(num > 0) || (num > std_list.Length));
 
@@ -94,7 +109,7 @@ namespace Student_list_Program
                             Array.Resize(ref std_list, std_list.Length - 1);
                         }
 
-                        Console.WriteLine("{0}번 학생을 성공적으로 삭제했습니다.",num);
+                        Console.WriteLine("{0}번 학생을 성공적으로 삭제했습니다.", num);
                         Console.ReadKey();
                     }
                 }
@@ -111,7 +126,6 @@ namespace Student_list_Program
                 if (std_list.Length <= 0)
                 {
                     Console.WriteLine("학생정보가 없습니다.");
-                    Console.ReadKey();
                 }
                 else
                 {
@@ -124,9 +138,9 @@ namespace Student_list_Program
                         j++;
                         Console.WriteLine("{0 }\t\t{1 }\t\t{2 }\t\t{3 }\t\t{4 }", j, student.name, student.kor, student.eng, student.math);
                     }
-                    
+
                 }
-                
+
             }
         }
 
