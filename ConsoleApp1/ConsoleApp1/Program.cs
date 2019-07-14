50766d88d106525cc32b338e0bd73230d5e3270b
jongseung <park930902@gmail.com>
2019-07-14 16:36:15 +0900

    프로그램 메뉴 구현
    for 문을 이용하여 메뉴 무한 반복

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,12 +12,47 @@ using System.Threading.Tasks;
  * 3. 프로그램 종료 시 학생 목록들을 파일로 저장
  * 4. 프로그램 시작 시 파일을 불러와 학생 목록을 로드
  */
-namespace Sturdent_list_Program
+namespace Student_list_Program
 {
+    class Student //Student 클래스 정의
+    {
+        public string name;
+        public int kor, eng, math;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            // 학생 관리 프로그램 메뉴
+            // 1. 학생 목록 2. 학생 추가 3.학생 삭제 4. 프로그램 종료
+            // 4. 프로그램 종료 메뉴에 들어갈때까지 무한 반복
+            int select = 0 ;
+
+            for(; select < 4 ;)
+            {
+                Console.WriteLine("1. 학생 목록 2. 학생 추가 3.학생 삭제 4. 프로그램 종료"); // 다른 번호를 눌러도 프로그램 종료
+                Console.Write("메뉴 번호를 입력하세요 : ");
+                select = int.Parse(Console.ReadLine()); //사용자 메뉴 선택
+
+                if (select == 1) // 1. 학생 목록
+                {
+
+                }
+                else if (select == 2) // 2. 학생 추가
+                {
+
+                }
+                else if (select == 3) // 3. 학생 삭제
+                {
+
+                }
+                else //프로그램 종료
+                {
+                    Console.WriteLine("프로그램을 종료합니다.");
+              
+                }
+            }
+            
         }
     }
 }
