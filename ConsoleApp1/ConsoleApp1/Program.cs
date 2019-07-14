7f97fae3a71abcd4f2d916de2267a5cc868191e1
jongseung <park930902@gmail.com>
2019-07-14 16:25:15 +0900

    학생관리프로그램 기능정리
    학생관리프로그램 기능정리 주석처리

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 학생 관리 프로그램
+ * 1. 학생관리 - 학생 이름, 국어, 영어, 수학 성적을 저장
+ *      - 학생 클래스 정의
+ * 2. 학생추가, 학생 목록 출력, 학생 정보 삭제
+ *      - Array.Resize(ref 배열변수, 변경할 요소 크기) 사용
+ * 3. 프로그램 종료 시 학생 목록들을 파일로 저장
+ * 4. 프로그램 시작 시 파일을 불러와 학생 목록을 로드
+ */
+namespace Sturdent_list_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
